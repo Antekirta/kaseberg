552c1eb5c9a312f49d2d14c93c8036d82168be59
Kirill Murashkin <murashkin@virtualhealth.com>
2021-07-30 14:10:41 +0200

    Add to basket tooltip

--- client/components/Product/AddToBasket.vue
+++ client/components/Product/AddToBasket.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="add-to-basket">
-    <b-button variant="dark" size="lg" @click="addToBasket">
+    <b-button id="add-to-basket" class="add-to-basket__btn" variant="dark" size="lg" @click="addToBasket">
       Добавить в корзину
     </b-button>
+
+    <b-tooltip
+      target="add-to-basket"
+      :title="tooltipTitle"
+      triggers="click"
+      :show="isTooltipVisible"
+      variant="dark"
+      placement="topright"
+    />
   </div>
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 import { MUTATIONS } from '@/store'
 
 export default {
@@ -20,12 +29,39 @@ export default {
     quantity: {
       type: Number,
       required: true
+    },
+    tooltipTimeout: {
+      type: Number,
+      default: 3000
     }
+  },
+  data () {
+    return {
+      isTooltipVisible: false,
+      tooltipTitle: 'Товар успешно добавлен в корзину! 🥳'
+    }
+  },
+  computed: {
+    ...mapState(['basket'])
   },
   methods: {
     ...mapMutations([MUTATIONS.M_ADD_TO_BASKET]),
 
     addToBasket () {
+      if (this.basket[this.productId]) {
+        this.tooltipTitle = 'А этот товар уже добавлен в корзину 😉 Вон она, в правом верхнем углу ↗'
+      } else {
+        this.tooltipTitle = 'Товар успешно добавлен в корзину! 🥳'
+      }
+
+      if (!this.isTooltipVisible) {
+        this.isTooltipVisible = true
+
+        setTimeout(() => {
+          this.isTooltipVisible = false
+        }, this.tooltipTimeout)
+      }
+
       this[MUTATIONS.M_ADD_TO_BASKET]({
         id: this.productId,
         quantity: this.quantity
@@ -34,3 +70,11 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.add-to-basket {
+  &__btn {
+    box-shadow: none!important;
+  }
+}
+</style>
